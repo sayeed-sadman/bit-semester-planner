3009a6424b6c5e6736679e94a0e15ad756f25c41
Sayeed Sadman <sayeed.sadman@yahoo.com>
2026-04-22 01:03:46 +0200

    Initial Spring Boot project setup with dependencies

---
+++ src/main/java/ch/fhnw/bitsemesterplanner/BitsemesterplannerApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.bitsemesterplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsemesterplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitsemesterplannerApplication.class, args);
+	}
+
+}
